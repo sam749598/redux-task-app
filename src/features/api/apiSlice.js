f094e7319b122019b235aae2be297940dcb713cf
ab sam <sam749598@gmail.com>
2026-03-24 21:46:01 +0600

    mock api base url set

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,9 +2,9 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 import { createApi } from "@reduxjs/toolkit/query/react";
 
 
-export const apiSlice=createApi({
-    reducerPath:"api",
-    baseQuery:fetchBaseQuery({baseUrl:"https://69c2ac2a7518bf8facbf2a8d.mockapi.io"}),
-    tagTypes:["Task"],
-    endpoints:(builder)=>({})
+export const apiSlice = createApi({
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({ baseUrl: "https://69c2ac2a7518bf8facbf2a8d.mockapi.io" }),
+    tagTypes: ["Task"],
+    endpoints: (builder) => ({})
 })
