d27420e24166fbeb6f48a2139b6cfc4b3ab06476
ab sam <sam749598@gmail.com>
2026-03-24 21:59:44 +0600

    cors fixing

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,10 +1,28 @@
+// import { fetchBaseQuery } from "@reduxjs/toolkit/query";
+// import { createApi } from "@reduxjs/toolkit/query/react";
+
+
+// export const apiSlice = createApi({
+//     reducerPath: "api",
+//     baseQuery: fetchBaseQuery({ baseUrl: "https://69c2ac2a7518bf8facbf2a8d.mockapi.io" }),
+//     tagTypes: ["Task"],
+//     endpoints: (builder) => ({})
+// })
+
+
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 import { createApi } from "@reduxjs/toolkit/query/react";
 
-
 export const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: "https://69c2ac2a7518bf8facbf2a8d.mockapi.io" }),
+    baseQuery: fetchBaseQuery({ 
+        baseUrl: "https://69c2ac2a7518bf8facbf2a8d.mockapi.io",
+        prepareHeaders: (headers) => {
+            headers.set("Content-Type", "application/json");
+            headers.set("Accept", "application/json");
+            return headers;
+        },
+    }),
     tagTypes: ["Task"],
-    endpoints: (builder) => ({})
-})
+    endpoints: (builder) => ({}),
+});
