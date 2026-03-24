1ad0e0cdcf658818e91e90a9ae41caba05f6570f
ab sam <sam749598@gmail.com>
2026-03-24 21:53:01 +0600

    css name fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,6 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import { Provider } from 'react-redux'
-// import { store } from './app/store.js'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <Provider store={store}>
-//         <App />
-//     </Provider>
-//   </StrictMode>,
-// )
-
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './app.css'  // ← was index.css, WRONG FILE
+import './App.css' 
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.js'
